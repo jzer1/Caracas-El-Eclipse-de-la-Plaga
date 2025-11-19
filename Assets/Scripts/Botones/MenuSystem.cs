8feba7a563aabe609dbee90e16e1ae3f19eddb6b
p4ch3z <arodriguez59@udi.edu.co>
2025-11-18 22:42:40 -0500

    pache

--- Assets/Scripts/Botones/MenuSystem.cs
+++ Assets/Scripts/Botones/MenuSystem.cs
@@ -7,8 +7,9 @@ public class MenuSystem : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Jugar()
     {
-        Debug.Log("Cargando la siguiente escena");
+        Debug.Log("Cargando la siguiente escena.");
         SceneManager.LoadScene("Partida");
+
     }
 
     public void Salir()
