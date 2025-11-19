6c2fd65a5efef6e3c8683eccdaee55d5e7c7f6ab
p4ch3z <arodriguez59@udi.edu.co>
2025-11-18 21:03:51 -0500

    Menu borrador

---
+++ Assets/Scripts/Botones/MenuSystem.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuSystem : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Jugar()
+    {
+        Debug.Log("Cargando la siguiente escena");
+        SceneManager.LoadScene("Partida");
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Salir del juego");
+        Application.Quit();
+    }
+}
