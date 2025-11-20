d287a5cd4559556649eec3c91e5bc8571641ca42
jzer1 <jzerpa1@udi.edu.co>
2025-11-20 00:45:44 -0500

    Revert "guardar y cargar partida"

--- Assets/Scripts/PlayerData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-[System.Serializable]
-public class PlayerData
-{
-    public int fragments; // Dinero/fragmentos
-    public int level; // Nivel si quieres guardar progreso
-    public List<string> unlockedMaps; // Nombres de escenas desbloqueadas
-    public List<string> unlockedCharacters; // Personajes desbloqueados
-}
